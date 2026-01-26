c48c7706066493d2baf5be6c20ebb1b78d5ae46e
mahmoudaymangomaa <ma3538463@gmail.com>
2026-01-26 04:01:55 +0200

    feat: add login and home components and configure application routing with lazy loading and an authentication guard.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./features/auth/login/login')
+    },
+    {
+        path: '',
+        canActivate: [authGuard],
+        loadComponent: () =>
+            import('./features/home/home/home')
+    },
+    //   {
+    //     path: 'products',
+    //     canActivate: [authGuard],
+    //     loadChildren: () =>
+    //       import('./features/products/products.routes')
+    //   },
+    //   {
+    //     path: 'invoices',
+    //     canActivate: [authGuard],
+    //     loadChildren: () =>
+    //       import('./features/invoices/invoices.routes')
+    //   },
+    { path: '**', redirectTo: '' }
+
+];
